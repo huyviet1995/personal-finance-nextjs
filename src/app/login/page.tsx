ccdea477d5301aa5b220e69effc2b15b88ba5c45
vietdh <vietdanghuy.95@gmail.com>
2024-11-14 23:53:26 +0700

    Add texts under the image

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,21 +5,23 @@ import Image from "next/image";
 const LoginPage = () => {
   return (
     <main className="flex flex-row h-[100vh]">
-      <section
-        className="p-4 w-50"
-      >
+      <section className="relative p-4">
         <Image
           src="/images/illustration-authentication.svg"
           alt="hero-image"
           width={'500'}
           height={'400'}
           objectFit="cover"
-          className="h-full rounded-lg w-50"
+          className="h-full w-full rounded-lg w-50"
         />
+        <div className="absolute p-4 bottom-10 w-full h-full flex flex-col justify-end items-center text-white">
+          <h1 className="text-[32px] font-bold">Keep track of your money and save for your future</h1>
+          <p className="text-xl mt-2">
+            Personal finance app puts you in control of your spending. Track transactions, set budgets, and add to savings pots easily.
+          </p>
+        </div>
       </section>
-      <section
-        className='flex flex-1 justify-center items-center'
-      >
+      <section className='flex flex-1 justify-center items-center'>
         <LoginForm />
       </section>
     </main>
