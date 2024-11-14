9cdd9a35994d04d20ebc767baf979101261647d6
vietdh <vietdanghuy.95@gmail.com>
2024-11-14 23:27:08 +0700

    [FE] Adding the left hand side iamge

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
-import { AuthForm as LoginForm } from '@/components/forms/auth-form';
+import React from "react";
+import { AuthForm as LoginForm } from "@/components/forms/auth-form";
+import Image from "next/image";
 
 const LoginPage = () => {
   return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <section style={{ flex: 1, backgroundImage: 'url(/path/to/your/image.jpg)', backgroundSize: 'cover' }}>
-        {/* Left column with image */}
+    <div style={{ display: "flex", height: "100vh" }}>
+      <section
+        className="p-4 h-full"
+      >
+        <Image
+          src="/images/illustration-authentication.svg"
+          alt="hero-image"
+          width={'500'}
+          height={'100'}
+          className="h-full w-auto rounded-lg"
+        />
       </section>
-      <section style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+      <section
+        style={{
+          flex: 1,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
         <LoginForm />
       </section>
     </div>
