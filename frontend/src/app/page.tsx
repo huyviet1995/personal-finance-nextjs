2b702bf9346c680e6c3ba20b1a313e82add3df9a
vietdh <vietdanghuy.95@gmail.com>
2024-11-19 22:58:30 +0700

    Remove unused console.log

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,6 @@ import useLocalStorage from "@/hooks/useLocalStorage";
 
 export default function Home() {
   const [user] = useLocalStorage('user', { username: '' });
-  console.log("🚀 ~ Home ~ user:", user)
   if (!user.username) return;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
